e8debcd831a8a1d17281acd792155f82af3fcf93
Super Cool Ninja <samir932205@gmail.com>
2022-06-23 20:45:10 +0200

    sourcecode

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # GTA_Marker
  A marker that show the distance of your waypoint like arma 3.
+
+
+## Description : 
+It draw you a marker with the distance of your waypoint set. 
+<br>render like Arma 3. 
+<br><br>inspirating from arma 3 altis life.
+
+## Usage
+So basicaly you only have to set a waypoint and it will draw you in screen the distance.
+
+
+## Installation :
+<ol>
+<li>go to the release folder</li>
+<li>drop the .asi file into your GTA V directory file</li>
+<li>enjoy.</li>
+</ol>
+
+
+## For Developer :
+Well, you will find all the sourcecode on the script.cpp file
+you can modify, contribute to the project to improve it but let the copyright.
+
+
+## Copyright
+Script made by **SuperCoolNinja** for **GTA V SOLO.**
+
+We are in no way affiliated with or endorsed by Rockstar Games or Take-Two Interactive.
+
+Grand Theft Auto V and Grand Theft Auto Online are trademarks of Rockstar Games, owned by Take Two Interactive.
---
+++ sourcecode/samples/GTA_Marker/script.cpp
@@ -0,0 +1,67 @@
+#include "script.h"
+#include <cmath>
+#include <string>
+
+void drawText3DMarker(float coordX, float coordY, float playerZ, char* text)
+{
+	float x, y;
+	if (GRAPHICS::_WORLD3D_TO_SCREEN2D(coordX, coordY, playerZ, &x, &y))
+	{
+		UI::SET_TEXT_SCALE(0.3 + 0.03, 0.3 + 0.03);
+		UI::SET_TEXT_FONT(0);
+		UI::SET_TEXT_PROPORTIONAL(TRUE);
+		UI::SET_TEXT_COLOUR(255, 255, 255, 255);
+		UI::SET_TEXT_DROPSHADOW(0, 0, 0, 0, 55);
+		UI::SET_TEXT_EDGE(2, 0, 0, 0, 150);
+		UI::SET_TEXT_OUTLINE();
+		UI::_SET_TEXT_ENTRY("STRING");
+		UI::SET_TEXT_CENTRE(TRUE);
+		UI::_ADD_TEXT_COMPONENT_STRING(text);
+		UI::_DRAW_TEXT(x, y);
+	}
+}
+
+float getDistance(float a_X, float a_Y, float a_Z, float b_X, float b_Y, float b_Z)
+{
+	auto x = a_X - b_X;
+	auto y = a_Y - b_Y;
+	auto z = a_Z - b_Z;
+	auto sumDist = x * x + y * y + z * z;
+	auto res = std::sqrt(sumDist);
+	return res;
+}
+
+
+//Main entry :
+void ScriptMain()
+{
+	char bufferOne[100];
+	char bufferTwo[100];
+
+	int timerOptimize = 1000;
+
+	while (true)
+	{
+		timerOptimize = 1000;
+
+		auto waypoint = UI::GET_FIRST_BLIP_INFO_ID(8);
+		if (UI::DOES_BLIP_EXIST(waypoint))
+		{
+			auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
+			auto coord = UI::GET_BLIP_INFO_ID_COORD(waypoint);
+			auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
+
+			if (distance > 999)
+			{
+				auto roundOverKM = std::round(distance) * std::pow(10, -3);
+				sprintf(bufferOne, "V\n %.1lf km", roundOverKM);
+			}
+			else sprintf(bufferOne, "V\n %ld m", (int)std::round(distance));
+			
+			drawText3DMarker(coord.x, coord.y, playerPos.z, bufferOne);
+			timerOptimize = 0;
+		}
+
+		WAIT(timerOptimize);
+	}
+}
