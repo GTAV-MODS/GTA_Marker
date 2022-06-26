40815e11e91900d4f13518a433a25c098566c9f4
Super Cool Ninja <samir932205@gmail.com>
2022-06-27 00:28:55 +0200

    added marker to mission objective.

--- sourcecode/samples/GTA_Marker/script.cpp
+++ sourcecode/samples/GTA_Marker/script.cpp
@@ -6,7 +6,7 @@
 /*Script made by SuperCoolNinja*/
 
 
-void drawText3DMarker(float coordX, float coordY, float playerZ, char* text)
+static void drawText3DMarker(float coordX, float coordY, float playerZ, char* text)
 {
 	float x, y;
 	if (GRAPHICS::_WORLD3D_TO_SCREEN2D(coordX, coordY, playerZ, &x, &y))
@@ -25,7 +25,7 @@ void drawText3DMarker(float coordX, float coordY, float playerZ, char* text)
 	}
 }
 
-float getDistance(float a_X, float a_Y, float a_Z, float b_X, float b_Y, float b_Z)
+static float getDistance(float a_X, float a_Y, float a_Z, float b_X, float b_Y, float b_Z)
 {
 	auto x = a_X - b_X;
 	auto y = a_Y - b_Y;
@@ -36,34 +36,73 @@ float getDistance(float a_X, float a_Y, float a_Z, float b_X, float b_Y, float b
 }
 
 
+static void showPlayerMarker(Vector3 playerPos, Vector3 coord, float distance,char text[])
+{
+	if (distance > 999)
+	{
+		auto roundOverKM = std::round(distance) * std::pow(10, -3);
+		sprintf(text, "V\n %.1lf km", roundOverKM);
+	}
+	else sprintf(text, "V\n %ld m", (int)std::round(distance));
+
+	drawText3DMarker(coord.x, coord.y, playerPos.z, text);
+}
+
 //Main entry :
 void ScriptMain()
 {
-	char bufferOne[100];
-	char bufferTwo[100];
+	char text[100];
+	int timerOptimize{1000};
+	auto blip{0 };
+	auto foundBlip{ false };
 
-	int timerOptimize = 1000;
-
+	
 	while (true)
 	{
 		timerOptimize = 1000;
 
-		auto waypoint = UI::GET_FIRST_BLIP_INFO_ID(8);
-		if (UI::DOES_BLIP_EXIST(waypoint))
+		//Check if the player is actually playing any mission : 
+		if (GAMEPLAY::GET_MISSION_FLAG())
 		{
-			auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
-			auto coord = UI::GET_BLIP_INFO_ID_COORD(waypoint);
-			auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
-
-			if (distance > 999)
+			if (foundBlip == false)
 			{
-				auto roundOverKM = std::round(distance) * std::pow(10, -3);
-				sprintf(bufferOne, "V\n %.1lf km", roundOverKM);
+				//Loop though the blip to find if it correspond to the player blip mission :
+				for (int i = 0; i < 826; i++)
+				{
+					blip = UI::GET_FIRST_BLIP_INFO_ID(i);
+
+					if (UI::DOES_BLIP_EXIST(blip) && UI::_0xDD2238F57B977751(blip))
+					{
+						foundBlip = true;
+						break;
+					}
+				}
 			}
-			else sprintf(bufferOne, "V\n %ld m", (int)std::round(distance));
-			
-			drawText3DMarker(coord.x, coord.y, playerPos.z, bufferOne);
-			timerOptimize = 0;
+			else
+			{
+				//If the player is not in mission then we don't need to loop but only check if the waypoint is set :
+				if (!UI::DOES_BLIP_EXIST(blip)) foundBlip = false;
+				else
+				{
+					auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
+					auto coord = UI::GET_BLIP_INFO_ID_COORD(blip);
+					auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
+					showPlayerMarker(playerPos, coord, distance, text);
+				}
+				timerOptimize = 0;
+			}
+		}
+		else
+		{
+			blip = UI::GET_FIRST_BLIP_INFO_ID(8);
+			if (UI::DOES_BLIP_EXIST(blip))
+			{
+				auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
+				auto coord = UI::GET_BLIP_INFO_ID_COORD(blip);
+				auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
+				showPlayerMarker(playerPos, coord, distance, text);
+				timerOptimize = 0;
+			}
 		}
 
 		WAIT(timerOptimize);
