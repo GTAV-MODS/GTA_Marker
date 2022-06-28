b991e865e4418b4028821125ca257e3d99cd45c2
SuperCoolNinja <samir932205@gmail.com>
2022-06-28 21:16:14 +0200

    Version stable, added all objective with gps route

--- sourcecode/samples/GTA_Marker/script.cpp
+++ sourcecode/samples/GTA_Marker/script.cpp
@@ -61,50 +61,36 @@ void ScriptMain()
 	{
 		timerOptimize = 1000;
 
-		//Check if the player is actually playing any mission : 
-		if (GAMEPLAY::GET_MISSION_FLAG())
+		if (foundBlip == false)
 		{
-			if (foundBlip == false)
+			//Loop though the blip to find if it correspond to the player blip mission :
+			for (int i = 0; i < 826; i++)
 			{
-				//Loop though the blip to find if it correspond to the player blip mission :
-				for (int i = 0; i < 826; i++)
-				{
-					blip = UI::GET_FIRST_BLIP_INFO_ID(i);
+				blip = UI::GET_FIRST_BLIP_INFO_ID(i);
+				bool checkIfBlipHaveRoute = (UI::DOES_BLIP_EXIST(blip) && (UI::_0xDD2238F57B977751(blip)));
+				bool isWaypointFound = (i == 8 && UI::DOES_BLIP_EXIST(UI::GET_FIRST_BLIP_INFO_ID(i)));
 
-					if (UI::DOES_BLIP_EXIST(blip) && UI::_0xDD2238F57B977751(blip))
-					{
-						foundBlip = true;
-						break;
-					}
-				}
-			}
-			else
-			{
-				//If the player is not in mission then we don't need to loop but only check if the waypoint is set :
-				if (!UI::DOES_BLIP_EXIST(blip)) foundBlip = false;
-				else
+				//Check if the blip waypoint is found then we exit the loop and show the waypoint marker distance: 
+				if (isWaypointFound || checkIfBlipHaveRoute)
 				{
-					auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
-					auto coord = UI::GET_BLIP_INFO_ID_COORD(blip);
-					auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
-					showPlayerMarker(playerPos, coord, distance, text);
+					foundBlip = true;
+					break;
 				}
-				timerOptimize = 0;
 			}
 		}
 		else
 		{
-			blip = UI::GET_FIRST_BLIP_INFO_ID(8);
-			if (UI::DOES_BLIP_EXIST(blip))
+			//If the player is not in mission then we don't need to loop but only check if the waypoint is set :
+			if (!UI::DOES_BLIP_EXIST(blip)) foundBlip = false;
+			else
 			{
 				auto playerPos = ENTITY::GET_ENTITY_COORDS(PLAYER::PLAYER_PED_ID(), TRUE);
 				auto coord = UI::GET_BLIP_INFO_ID_COORD(blip);
 				auto distance = getDistance(playerPos.x, playerPos.y, playerPos.z, coord.x, coord.y, coord.z);
 				showPlayerMarker(playerPos, coord, distance, text);
-				timerOptimize = 0;
 			}
+			timerOptimize = 0;
 		}
-
 		WAIT(timerOptimize);
 	}
 }
